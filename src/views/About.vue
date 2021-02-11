d717cb63f0aeb8bca0942c8e82a61f30af5abdb5
jeanpaulHamuli <jeanpaulhamuli@gmail.com>
2021-02-11 18:26:56 +0100

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,62 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div>
+        <p>Jean-Paul</p>
+        <div id="chartDiv" style="width: 100%; height: 400px;"></div>
+    </div>
 </template>
+ 
+<script>
+    import * as JSC from "jscharting";
+ 
+    JSC.defaults({ baseUrl: './js/jsc/' });
+ 
+export default {
+    mounted(){
+        this.graphic();
+    },
+     methods: {
+        graphic: function(){
+            const chart = new JSC.Chart("chartDiv", {
+                debug: false, 
+                type: "gauge" , 
+                legend_visible: false, 
+                yAxis: { 
+                  line_visible: false, 
+                  defaultTick_label_text: '%value%', 
+                  scale_range: [0, 100] 
+                }, 
+                defaultSeries: { 
+                  type: 'column roundCaps', 
+                  angle: { sweep: 360, start: -90 }, 
+                  shape: { 
+                    innerSize: '70%', 
+                    padding: 0.1, 
+                    label: { 
+                      text: 
+                        'Notice exceeding activity<br/>goals at a glance', 
+                      verticalAlign: 'middle'
+                    } 
+                  } 
+                }, 
+                defaultTooltip_enabled: false, 
+                series: [ 
+                  { 
+                    palette: [ 
+                      '#E10715', 
+                      '#3FDC00', 
+                      '#3EBAE1'
+                    ], 
+                    defaultPoint: { label_text: '%yValue%' }, 
+                    points: [['value', 5]] 
+                  } 
+                ], 
+                toolbar: { 
+                  defaultItem: { position: 'top' }, 
+                } 
+            });
+        },
+        
+     }
+}
+</script> 
+ 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Welcome to  App" />
+   
   </div>
 </template>
 
@@ -9,10 +10,14 @@
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
 
+
+
+
 export default {
   name: "Home",
   components: {
     HelloWorld
-  }
+  },
+  
 };
 </script>
